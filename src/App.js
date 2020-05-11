cf99ef7b3088769d1aa04c2830f3114f23c44ccd
elywin <lukeeraelywin@gmail.com>
2020-05-12 02:23:23 +0300

    change in file path

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends React.Component{
+ 
+  render(){
+    return(
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Change value when button is clicked</code>         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+       <div className="btn">
+        <button >Michael</button>
+        <button >Elywin</button>
+        </div>
+      </header>
+    </div>
+
+    );
+  }
+}
+
+export default App;
